c664360899927bc0e8a53f21504969feece5ea28
BryCur <bryan.curchod@gmail.com>
2024-12-05 15:07:16 +0100

    day 4 done

---
+++ aocUtils/ArrayExtensions.cs
@@ -0,0 +1,15 @@
+﻿public static class ArrayExtensions
+{
+    public static T[,] To2D<T>(this T[][] source)
+    {
+        int rows = source.Length;
+        int cols = source[0].Length;
+        T[,] result = new T[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                result[i, j] = source[i][j];
+
+        return result;
+    }
+}
--- day-04/Program.cs
+++ day-04/Program.cs
@@ -5,10 +5,15 @@ using aocUtils.IO;
 
 public class Day04
 {
-    private const string DEFAULT_INPUT_FILE = "./inputs/example.txt";
+    private const string DEFAULT_INPUT_FILE = "./inputs/real-input.txt";
     private const string WANTED_WORD = "XMAS";
     
     private string inputFile;
+    
+    private List<string> lines = new List<string>();
+    private List<(int x, int y)> xPostitions = new List<(int, int)>();
+    private List<(int x, int y)> aPostitions = new List<(int, int)>();
+    private char[,] table;
     
     public static void Main(string[] args)
     {
@@ -39,6 +44,7 @@ public class Day04
     {
         this.inputFile = inputFile;
         parseInput();
+        table = initTable();
     }
 
     private void parseInput()
@@ -49,19 +55,212 @@ public class Day04
 
     public void parseLine(string line)
     {
-        Console.WriteLine(line);
-        line.in
+        int xPos = lines.Count;
+        addPositionsOfCharToList(line, WANTED_WORD[0], xPostitions, xPos);
+        addPositionsOfCharToList(line, 'A', aPostitions, xPos);
+        lines.Add(line);
+    }
+
+    private void addPositionsOfCharToList(string line, char seeked, List<(int, int)> positionList, int xPos)
+    {
+        int yPos = 0;
+        while (line.IndexOf(seeked, yPos) != -1)
+        {
+            yPos = line.IndexOf(seeked, yPos);
+            positionList.Add((xPos, yPos));
+            yPos++;
+        }
     }
 
     public void part1()
     {
         int result = 0;
+        foreach ((int, int) xPos in xPostitions)
+        {
+            result += checkRight(xPos)? 1:0;
+            result += checkLeft(xPos)? 1:0;
+            result += checkUp(xPos)? 1:0;
+            result += checkDown(xPos)? 1:0;
+            result += checkDiagTopRight(xPos)? 1:0;
+            result += checkDiagBottomRight(xPos)? 1:0;
+            result += checkDiagTopLeft(xPos)? 1:0;
+            result += checkDiagBottomLeft(xPos)? 1:0;
+        }
         Console.WriteLine($"part 1 solution: {result}");
     }
     
     public void part2()
     {
         int result = 0;
+
+        foreach ((int x, int y) pos in aPostitions)
+        {
+            result += checkCrossMAS(pos)? 1:0;
+        }
         Console.WriteLine($"part 2 solution: {result}");
+    }
+
+    private char[,] initTable()
+    {
+        return lines
+            .Select(s => s.ToCharArray())
+            .ToArray()
+            .To2D();
+    }
+
+
+    private bool checkLeft((int x, int y) pos)
+    {
+        if(pos.y + 3 < table.GetLength(1))
+        {
+            char[] tested = new []
+            {
+                table[pos.x, pos.y], 
+                table[pos.x, pos.y+1],
+                table[pos.x, pos.y+2], 
+                table[pos.x, pos.y+3]
+            };   
+            
+            return new string(tested).Equals(WANTED_WORD);
+        }
+        return false;
+    }
+    
+    private bool checkRight((int x, int y) pos)
+    {
+        if(pos.y - 3 >= 0)
+        {
+            char[] tested = new []
+            {
+                table[pos.x, pos.y], 
+                table[pos.x, pos.y-1],
+                table[pos.x, pos.y-2], 
+                table[pos.x, pos.y-3]
+            };   
+            
+            return new string(tested).Equals(WANTED_WORD);
+        }
+        return false;
+    }
+    
+    private bool checkUp((int x, int y) pos)
+    {
+        if(pos.x - 3 >= 0)
+        {
+            char[] tested = new []
+            {
+                table[pos.x, pos.y], 
+                table[pos.x-1, pos.y],
+                table[pos.x-2, pos.y], 
+                table[pos.x-3, pos.y]
+            };   
+            
+            return new string(tested).Equals(WANTED_WORD);
+        }
+        return false;
+    }
+    
+    private bool checkDown((int x, int y) pos)
+    {
+        if(pos.x + 3 < table.GetLength(0))
+        {
+            char[] tested = new []
+            {
+                table[pos.x, pos.y], 
+                table[pos.x+1, pos.y],
+                table[pos.x+2, pos.y], 
+                table[pos.x+3, pos.y]
+            };   
+            
+            return new string(tested).Equals(WANTED_WORD);
+        }
+        return false;
+    }
+    
+    private bool checkDiagTopLeft((int x, int y) pos)
+    {
+        if(pos.x - 3 >= 0 && pos.y - 3 >= 0)
+        {
+            char[] tested = new []
+            {
+                table[pos.x, pos.y], 
+                table[pos.x-1, pos.y-1],
+                table[pos.x-2, pos.y-2], 
+                table[pos.x-3, pos.y-3]
+            };   
+            
+            return new string(tested).Equals(WANTED_WORD);
+        }
+        return false;
+    }
+    
+    private bool checkDiagTopRight((int x, int y) pos)
+    {
+        if(pos.x - 3 >= 0 && pos.y + 3 < table.GetLength(1))
+        {
+            char[] tested = new []
+            {
+                table[pos.x, pos.y], 
+                table[pos.x-1, pos.y+1],
+                table[pos.x-2, pos.y+2], 
+                table[pos.x-3, pos.y+3]
+            };   
+            
+            return new string(tested).Equals(WANTED_WORD);
+        }
+        return false;
+    }
+    
+    private bool checkDiagBottomRight((int x, int y) pos)
+    {
+        if(pos.x + 3 < table.GetLength(0) && pos.y + 3 < table.GetLength(1))
+        {
+            char[] tested = new []
+            {
+                table[pos.x, pos.y], 
+                table[pos.x+1, pos.y+1],
+                table[pos.x+2, pos.y+2], 
+                table[pos.x+3, pos.y+3]
+            };   
+            
+            return new string(tested).Equals(WANTED_WORD);
+        }
+        return false;
+    }
+    
+    private bool checkDiagBottomLeft((int x, int y) pos)
+    {
+        if(pos.x + 3 < table.GetLength(0) && pos.y - 3 >= 0)
+        {
+            char[] tested = new []
+            {
+                table[pos.x, pos.y], 
+                table[pos.x+1, pos.y-1],
+                table[pos.x+2, pos.y-2], 
+                table[pos.x+3, pos.y-3]
+            };   
+            
+            return new string(tested).Equals(WANTED_WORD);
+        }
+        return false;
+    }
+
+    private bool checkCrossMAS((int x, int y) pos)
+    {
+        HashSet<string> validSequence = new HashSet<string>() { "MMSS", "SMSM", "SSMM", "MSMS" };
+        if (0 < pos.x && pos.x < table.GetLength(0) - 1 && 0 < pos.y && pos.y < table.GetLength(1) - 1)
+        {
+            char[] tested = new []
+            {
+                table[pos.x-1, pos.y-1], 
+                table[pos.x-1, pos.y+1],
+                table[pos.x+1, pos.y-1], 
+                table[pos.x+1, pos.y+1]
+            };   
+            
+            return validSequence.Contains(new string(tested));
+        }
+
+        return false;
     }
 }
