8cf2e1d78ba4676c7ffb0e9152c25e0a55c8684a
BryCur <bryan.curchod@gmail.com>
2024-12-19 22:29:08 +0100

    maybe I forgot something

--- aocUtils/LinkedNode.cs
+++ aocUtils/LinkedNode.cs
@@ -5,12 +5,14 @@ namespace aocUtils;
 public class LinkedNode<T>(T val)
 {
     private T Value => val;
-    private HashSet<LinkedNode<T>> AdjacentNodes = new HashSet<LinkedNode<T>>();
+    protected HashSet<LinkedNode<T>> AdjacentNodes = new HashSet<LinkedNode<T>>();
     
     public bool AddAdjacentNode(LinkedNode<T> node)
     {
         return this.AdjacentNodes.Add(node);
     }
+    
+    public HashSet<LinkedNode<T>> GetAdjacentNodes() => AdjacentNodes;
     
     public T GetValue() => this.Value;
 }
