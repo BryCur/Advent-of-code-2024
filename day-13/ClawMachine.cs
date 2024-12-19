64d92867885ec0f365549c2c8d148d08cf08ba84
BryCur <bryan.curchod@gmail.com>
2024-12-19 22:28:44 +0100

    day 13 - done

---
+++ day-13/ClawMachine.cs
@@ -0,0 +1,68 @@
+﻿using aocUtils;
+
+namespace day_13;
+
+public class ClawMachine
+{
+    private Coordinate2D buttonA;
+    private Coordinate2D buttonB;
+    private Coordinate2D target;
+
+    private decimal clickA;
+    private decimal clickB;
+
+    public ClawMachine(Coordinate2D buttonA, Coordinate2D buttonB, Coordinate2D target)
+    {
+        this.buttonA = buttonA;
+        this.buttonB = buttonB;
+        this.target = target;
+
+        GetSolution();
+    }
+
+    public bool IsTargetAccessible()
+    {
+        return decimal.IsInteger(clickA) && decimal.IsInteger(clickB);
+    }
+
+    public void GetSolution()
+    {
+        
+        /*
+         * solution for
+         * c: coins for btnA
+         * d: coins for btnB
+         * Ax/Ay : offset on x/y axis of btn A
+         * Bx/By : offset on x/y axis of btn B
+         * Tx/Ty : x/y position of target
+         *
+         * 1) cAx + dBx = Tx
+         * 2) cAy + dBy = Ty
+         */
+        clickB = (decimal)(target.getY() * buttonA.getX() - buttonA.getY() * target.getX()) / (buttonA.getX() * buttonB.getY() - buttonB.getX() * buttonA.getY());
+        clickA = (target.getX() - clickB * buttonB.getX()) / buttonA.getX();
+    }
+    
+    public (decimal ACoins, decimal BCoins) GetSolutionWithOffset(decimal offset)
+    {
+        decimal newXTarget = target.getX() + offset;
+        decimal newYTarget = target.getY() + offset;
+
+        decimal determinant = (buttonA.getX() * buttonB.getY()) - (buttonB.getX() * buttonA.getY());
+        decimal aNum = (buttonB.getY() * newXTarget) - (newYTarget * buttonB.getX());
+        decimal bNum = ((newYTarget * buttonA.getX()) - (buttonA.getY() * newXTarget));
+
+        if (bNum % determinant is not 0 && aNum % determinant is not 0) return ((decimal)0.5, (decimal)0.5);
+        decimal newClickB = bNum / determinant;
+        decimal newClickA2 = aNum / determinant;
+        
+        return (newClickA2, newClickB);
+    }
+    
+    public decimal GetClickA() => clickA;
+    public decimal GetClickB() => clickB;
+    
+    public Coordinate2D GetButtonA() => buttonA;
+    public Coordinate2D GetButtonB() => buttonB;
+    public Coordinate2D GetTarget() => target;
+}
---
+++ day-13/Program.cs
@@ -0,0 +1,112 @@
+﻿using aocUtils;
+using aocUtils.IO;
+using day_13;
+
+public class Day13
+{
+    private const string DEFAULT_INPUT_FILE = "./inputs/real-input.txt";
+    
+    private string InputFile;
+    
+    private List<string> Input = new List<string>();
+    private List<ClawMachine> ClawMachines = new List<ClawMachine>();
+
+    private Coordinate2D currentAButton;
+    private Coordinate2D currentBButton;
+    private Coordinate2D currentTarget;
+    
+    public static void Main(string[] args)
+    {
+        string input;
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Usage: dayXX <input file>");
+            Console.WriteLine($"Using default input file: {DEFAULT_INPUT_FILE}");
+            input = DEFAULT_INPUT_FILE;
+        }
+        else
+        {
+            input = args[0];
+        }
+        
+        DateTime startTime = DateTime.Now;
+        
+        Day13 day13 = new Day13(input);
+        
+        day13.part1();
+        day13.part2();
+        
+        double timeElapsed = (DateTime.Now - startTime).TotalMilliseconds;
+        Console.WriteLine($"Time elapsed: {timeElapsed}");
+        
+    }
+
+    public Day13(String inputFile)
+    {
+        this.InputFile = inputFile;
+        parseInput();
+    }
+
+    private void parseInput()
+    {
+        TextFileReader.readFile(InputFile, parseLine);
+        // parse the last machine bc the file finishes on a "prize" line
+        ClawMachines.Add(new ClawMachine(currentAButton, currentBButton, currentTarget));
+        
+        Console.WriteLine("input processed");
+    }
+
+    public void parseLine(string line)
+    {
+        if (line.Contains("Button A:"))
+        {
+            currentAButton = parseCoordinate(line);
+        } else if (line.Contains("Button B:"))
+        {
+            currentBButton = parseCoordinate(line);
+        } else if (line.Contains("Prize:"))
+        {
+            currentTarget = parseCoordinate(line);
+        }
+        else
+        {
+            ClawMachines.Add(new ClawMachine(currentAButton, currentBButton, currentTarget));
+        }
+        
+        Input.Add(line);
+    }
+
+    private Coordinate2D parseCoordinate(string line)
+    {
+        string splitted =  line.Split(":")[1].Trim();
+        string[] coordinates = splitted.Split(",");
+
+        int x = int.Parse(coordinates[0].Trim().Substring(2));
+        int y = int.Parse(coordinates[1].Trim().Substring(2));
+
+        return new Coordinate2D(x, y);
+    }
+
+    public void part1()
+    {
+        long result = ClawMachines
+            .Where(c => c.IsTargetAccessible())
+            .Aggregate(0L, (i, machine) => i += (long)machine.GetClickA()*3L + (long)machine.GetClickB());
+        
+        Console.WriteLine($"part 1 solution: {result}");
+    }
+    
+    public void part2()
+    {
+        long offset = 10000000000000; // 10'000'000'000'000 => 10 trillion (en) / 10 billion (fr)
+
+        decimal result = ClawMachines
+            .Select(c => c.GetSolutionWithOffset(offset))
+            .Where(((decimal aCoins, decimal bCoins) t) => decimal.IsInteger(t.aCoins) && decimal.IsInteger(t.bCoins))
+            .Aggregate(0L, (decimal i, (decimal aCoins, decimal bCoins) t) => i += (long)t.aCoins*3L + (long)t.bCoins);
+
+        Console.WriteLine($"part 2 solution: {result}"); 
+    }
+}
+
+
