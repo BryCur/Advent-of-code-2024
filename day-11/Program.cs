b5e961a7468c193d31d1ecf7981066923765dcd3
BryCur <bryan.curchod@gmail.com>
2024-12-26 23:58:06 +0100

    day 11 - finished

--- day-11/Program.cs
+++ day-11/Program.cs
@@ -8,7 +8,8 @@ using day_11;
 public class Day11
 {
     private const string DEFAULT_INPUT_FILE = "./inputs/real-input.txt";
-    private const int MAX_BLINKS = 75;
+    private const int MAX_BLINKS_PART1 = 25;
+    private const int MAX_BLINKS_PART2 = 75;
     
     private string InputFile;
     
@@ -69,7 +70,7 @@ public class Day11
         
         List<long> result = new List<long>(Input);
         
-        for (int i = 0; i < MAX_BLINKS; i++)
+        for (int i = 0; i < MAX_BLINKS_PART1; i++)
         {
             for (int j = 0; j < result.Count; j++)
             {
@@ -88,9 +89,68 @@ public class Day11
     
     public void part2()
     {
-        int result = 0;
+        // who cares about the list, since we don't care about ordering... Counting the stones are the important part
+        // so a dictionary with the stone number as key, and the count of that number as value does the trick
+        Dictionary<long, long> dictStones = new Dictionary<long, long>();
+        foreach (long stone in Input)
+        {
+            if (!dictStones.ContainsKey(stone))
+            {
+                dictStones.Add(stone, 1);
+            }
+            else
+            {
+                dictStones[stone]++;
+            }
+        }
+
+        for(int i = 0; i < MAX_BLINKS_PART2; i++)
+        {
+            Dictionary<long, long> nextState = new Dictionary<long, long>();
+
+            foreach (KeyValuePair<long, long> kvp in dictStones)
+            {
+                long stoneNumber = kvp.Key;
+
+                if (stoneNumber == 0)
+                { // rule 1
+                    AddToDictIfNotExist(nextState, 1, kvp.Value);
+                } else if ((long)(Math.Log10(stoneNumber) + 1) % 2L == 0)
+                { // rule 2
+                    long digitCount = (long)(Math.Log10(stoneNumber) + 1);
+
+                    long left = stoneNumber / (long)Math.Pow(10, digitCount/2);
+                    long right = stoneNumber % (long)Math.Pow(10, digitCount/2);
+                    
+                    AddToDictIfNotExist(nextState, left, kvp.Value);
+                    AddToDictIfNotExist(nextState, right, kvp.Value);
+
+                }
+                else
+                { // rule 3
+                    AddToDictIfNotExist(nextState, stoneNumber * 2024, kvp.Value);
+                }
+            }
+            
+            dictStones = nextState;
+            
+        }
+
+        long result = dictStones.Values.Sum(); // sum all the values to have the count of stones
 
         Console.WriteLine($"part 2 solution: {result}");
+    }
+
+    private void AddToDictIfNotExist(Dictionary<long, long> dict, long key, long value)
+    {
+        if (!dict.ContainsKey(key))
+        {
+            dict.Add(key, value);
+        }
+        else
+        {
+            dict[key] += value;
+        }
     }
     
 }
